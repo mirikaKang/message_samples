44c5d308b553d70fce88ee69aa69b213b844b120
kcenon <kcenon@naver.com>
2022-07-22 16:28:12 +0900

    Preparing messaging_client by python

---
+++ echo_client.py
@@ -0,0 +1,24 @@
+from messaging_system.network.network import *
+
+server_address = ('127.0.0.1', 9876)
+client = messaging_client('echo_network', 231, 67)
+client.start(server_address)
+client.send_packet('@header={'
+                    '[1,unknown];'
+                    '[2,127.0.0.1:9876];'
+                    '[3,echo_client];'
+                    '[4,];'
+                    '[5,request_connection];'
+                    '[6,1.0.0.0];'
+                '};'
+                '@data={'
+                    '[connection_key,d,echo_network];'
+                    '[auto_echo,1,false];'
+                    '[auto_echo_interval_seconds,3,1];'
+                    '[session_type,2,1];'
+                    '[bridge_mode,1,false];'
+                    '[snipping_targets,e,0];'
+                '};')
+
+type_code, message = client.recv()
+client.stop()
