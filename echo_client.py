9bc5386fde6fe9451d3c6bd53a469024fcb16550
kcenon <kcenon@naver.com>
2022-07-26 15:48:34 +0900

    Changed echo message display

--- echo_client.py
+++ echo_client.py
@@ -7,8 +7,14 @@ client = messaging_client('echo_client', 'echo_network', 231, 67)
 client.start('127.0.0.1', 9876)
 
 echo_packet = container()
-echo_packet.create('', '',
-                    'echo_server', '', 'echo_test', [])
+echo_packet.create('', '', 'echo_server', '', 'echo_test')
 client.send_packet(echo_packet)
-print(client.recv_packet().serialize())        
+
+message = client.recv_packet()
+if message.message_type != "echo_test":
+    client.stop()
+    exit()
+
+print('received echo message from server')
+
 client.stop()
